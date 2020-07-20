19a96c4bf414a815cfb357edef403acd61873782
Ivan Seibel <ivanseibel@gmail.com>
2020-07-20 18:24:09 -0300

    Implement create customers methods

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,12 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createUser = container.resolve(CreateCustomerService);
+
+    const customer = await createUser.execute({ name, email });
+
+    return response.status(200).json(customer);
   }
 }
--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const emailInUse = await this.customersRepository.findByEmail(email);
+
+    if (emailInUse) {
+      throw new AppError('This email is already in use.');
+    }
+
+    const user = await this.customersRepository.create({
+      email,
+      name,
+    });
+
+    return user;
   }
 }
 
