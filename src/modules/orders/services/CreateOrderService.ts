6066c76f5fa07dce98fea1640ac8faf6bf36b8c9
Ivan Seibel <ivanseibel@gmail.com>
2020-07-21 09:19:03 -0300

    Implement execute method.

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,63 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Customer does not exist.');
+    }
+
+    const productsIds = products.map(product => ({ id: product.id }));
+    const productsMatch = await this.productsRepository.findAllById(
+      productsIds,
+    );
+
+    if (productsMatch.length !== products.length) {
+      throw new AppError('Some of requested products  does not exist.');
+    }
+
+    const productsToUpdateQuantity: IProduct[] = [];
+
+    const orderProducts = productsMatch.map(productMatch => {
+      const requestedProduct = products.find(product => {
+        return product.id === productMatch.id && product.quantity;
+      });
+
+      if (productMatch.quantity < (requestedProduct?.quantity || 0)) {
+        throw new AppError(`Product is out of stock: ${productMatch.name}.`);
+      }
+
+      productsToUpdateQuantity.push({
+        id: productMatch.id,
+        quantity: productMatch.quantity - (requestedProduct?.quantity || 0),
+      });
+
+      return {
+        product_id: productMatch.id,
+        price: productMatch.price,
+        quantity: requestedProduct?.quantity || 0,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: orderProducts,
+    });
+
+    await this.productsRepository.updateQuantity(productsToUpdateQuantity);
+
+    return order;
   }
 }
 
